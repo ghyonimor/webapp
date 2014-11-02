1aaa85eadfbcf39056702c428786f0a5cada2456
Yoni Mor <yoni-mor@users.noreply.github.com>
2014-11-02 18:24:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* globals UTILS*/
-UTILS.ajax('/data/notification.txt', {
+UTILS.ajax('data/notification.txt', {
 	method: 'GET',
 	done: function (response) {
 		if (response && response !== '') {
