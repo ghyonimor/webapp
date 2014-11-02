934cfbc3d75acf3f159e4db702130e25457ef4d7
Yoni Mor <yonimor89@hotmail.co.il>
2014-11-02 20:56:55 +0200

    keyboard tabs

--- js/main.js
+++ js/main.js
@@ -21,6 +21,27 @@ UTILS.ajax('../Web-App/data/notification.txt', {
  * Display / hide tab panels on tab click using addEvent function (keyboard too).
  * First getElementsByClassName('tab').
  */
+var display = function(e) {
+	e.preventDefault();
+	var activeTab = document.getElementsByClassName('activeTab')[0];
+	var activePanel = document.getElementsByClassName('activePanel')[0];
+	if (this.id !== activeTab.id) {
+		activeTab.className = activeTab.className.replace(/\bactiveTab\b/,'');
+		activePanel.className = activePanel.className.replace(/\bactivePanel\b/,'');
+		this.className = this.className + ' activeTab';
+		var anchor = this.getAttribute('href').replace('#', '');
+		var tabPanel = document.getElementById(anchor);
+		tabPanel.className = tabPanel.className + ' activePanel';
+		window.location.hash = 'panel-' + anchor;
+	}
+};
+
+var isEnter = function(e) {
+    if (event.which === 13 || event.keyCode === 13) {
+        display(e);
+    }
+};
+
 var tabs = document.getElementsByClassName('tab');
 for (var i = 0; i < tabs.length; i++) {
 	var tab = tabs[i];
@@ -31,20 +52,8 @@ for (var i = 0; i < tabs.length; i++) {
 		tabPanel.className = tabPanel.className + ' activePanel';
 		window.location.hash = 'panel-' + anchor;
 	}
-	addEvent(tab, 'click', function display(e) {
-		e.preventDefault();
-		var activeTab = document.getElementsByClassName('activeTab')[0];
-		var activePanel = document.getElementsByClassName('activePanel')[0];
-		if (this.id !== activeTab.id) {
-			activeTab.className = activeTab.className.replace(/\bactiveTab\b/,'');
-			activePanel.className = activePanel.className.replace(/\bactivePanel\b/,'');
-			this.className = this.className + ' activeTab';
-			var anchor = this.getAttribute('href').replace('#', '');
-			var tabPanel = document.getElementById(anchor);
-			tabPanel.className = tabPanel.className + ' activePanel';
-			window.location.hash = 'panel-' + anchor;
-		}
-	});
+	addEvent(tab, 'click', display);
+	addEvent(tab, 'keypress', isEnter);
 }
 
 
