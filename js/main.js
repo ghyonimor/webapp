12de5267e2bd0e30da1371ef0559c046cf1c0319
Yoni Mor <yonimor89@hotmail.co.il>
2014-11-02 16:18:34 +0200

    AJAX notification.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* globals UTILS*/
+UTILS.ajax('../data/notification.txt', {
+	method: 'GET',
+	done: function (response) {
+		if (response && response !== '') {
+			var message = document.getElementsByClassName('notifications')[0];
+			message.innerHTML = response;
+			message.style.display = 'block';
+		}
+	},
+	fail: function (err) {
+		console.log(err.message);
+	}
+});
